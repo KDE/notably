e16d825d07460c61ec72b342f6cca52defb1375e
Vishesh Handa <handa.vish@gmail.com>
2011-09-02 00:09:32 +0530

    Give credit to Eike Hein
    I have blatantly copied a lot of code from Yakuake.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,9 @@
    This file is part of the Nepomuk Notes project.
    Copyright (C) 2011  Vishesh Handa <handa.vish@gmail.com>
 
+   This code has been adapted from Yakuake's code
+   Copyright (C) 2008-2011 by Eike Hein <hein@kde.org>
+
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
    published by the Free Software Foundation; either version 2 of
